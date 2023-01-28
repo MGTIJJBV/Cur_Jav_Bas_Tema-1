f552e6085f70be9c26bc4642d91157463ab55f3c
iscjj <iscjjbv@outlook.com>
2023-01-27 18:20:18 -0600

    Class main

--- src/main/java/com/tema1/Main.java
+++ src/main/java/com/tema1/Main.java
@@ -2,6 +2,17 @@ package com.tema1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int ent = 0;
+        long lon = 3578923453673246L;
+        double dec = 2.99;
+        boolean flag = true;
+        String cadena = "Esto es una cadena ejemplo";
+
+        System.out.println("Valor entero int = " + ent);
+        System.out.println("Valor largo long = " + lon);
+        System.out.println("Valor decimal double = " + dec);
+        System.out.println("Valor boleano boolean  = " + flag);
+        System.out.println("Valor cadena String = " + cadena);
     }
 }
